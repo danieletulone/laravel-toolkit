0b51bd08715a0abbc88eca08b81fcdba5b29a82b
danieletulone <danieletulone.work@gmail.com>
2023-06-19 16:16:50 +0200

    update doc in DisablePrimaryKeyRequirement

--- src/Listeners/DisablePrimaryKeyRequirement.php
+++ src/Listeners/DisablePrimaryKeyRequirement.php
@@ -5,9 +5,29 @@ namespace Danieletulone\LaravelToolkit\Listeners;
 use Illuminate\Support\Facades\DB;
 
 /**
- * This event listener is used to disable the requirement
- * for tables in the database to have a primary key defined.
- * Required by DigitalOcean to work correctly.
+ * This event listener is used to disable the primary key requirement on migrations.
+ * 
+ * The SQL statement SET SESSION sql_require_primary_key=0 is used to disable the requirement
+ * for a primary key in certain database management systems (DBMS), particularly in cloud databases.
+ * By default, many relational database systems enforce the presence of a primary key on each table
+ * as a means of ensuring data integrity and facilitating efficient indexing.
+ *
+ * However, there may be scenarios where you might want to disable this requirement temporarily or permanently.
+ *
+ *1. Add the use:
+ *
+ * ```php
+ * use App\Listeners\DisablePrimaryKeyRequirement;
+ * use Illuminate\Database\Events\MigrationStarted;
+ * ```
+ * 
+ * 2. Add this lines in `$listen` array:
+ * 
+ * ```php
+ * MigrationStarted::class => [
+ *     DisablePrimaryKeyRequirement::class,
+ * ]
+ * ```
  */
 class DisablePrimaryKeyRequirement
 {
