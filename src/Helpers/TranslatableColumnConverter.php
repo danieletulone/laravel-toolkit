d36bf3277c7d2382697d0e77bc54b0d36a8a47cd
danieletulone <danieletulone.work@gmail.com>
2023-06-19 16:16:00 +0200

    fix TranslatableColumnConverter: now work with model also if there are mutators and global scopes

--- src/Helpers/TranslatableColumnConverter.php
+++ src/Helpers/TranslatableColumnConverter.php
@@ -4,23 +4,41 @@ namespace Danieletulone\LaravelToolkit\Helpers;
 
 class TranslatableColumnConverter
 {
+    /**
+     * Apply the conversion from string to json
+     * 
+     * TODO add check for model class
+     * TODO add check for column type
+     * 
+     * @param mixed $model The model class
+     * @param mixed $column The column to convert
+     * @return void 
+     */
     public static function apply($model, $column)
     {
-        $model::all()->each(function ($model) use ($column) {
-            $model->{$column} = json_encode([
-                'en' => $model->{$column},
-            ]);
+        $model::withoutGlobalScopes()
+            ->get()
+            ->each(function ($model) use ($column) {
+                $model->setRawAttributes([
+                    "$column" => [
+                        'en' => $model->{$column}
+                    ],
+                ]);
 
-            $model->save();
-        });
+                $model->save();
+            });
     }
 
-    public static function revert($model, $column)
+    public static function reverse($model, $column)
     {
-        $model::all()->each(function ($model) use ($column) {
-            $model->{$column} = json_decode($model->getRawOriginal($column))->en;
+        $model::withoutGlobalScopes()
+            ->get()
+            ->each(function ($model) use ($column) {
+                $model->setRawAttributes([
+                    "$column" => json_decode($model->getRawOriginal($column))->en
+                ]);
 
-            $model->save();
-        });
+                $model->save();
+            });
     }
 }
