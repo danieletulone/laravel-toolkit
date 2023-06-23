385d7be2c03cec8c36c171d414ceadb9d72734f6
danieletulone <danieletulone.work@gmail.com>
2023-06-23 14:56:20 +0200

    add new values to config

--- config/config.php
+++ config/config.php
@@ -5,4 +5,8 @@
  */
 return [
     'prefix' => env('LARAVEL_TOOLKIT_PREFIX', config('app.env', 'local')),
+    'notification' => [
+        'notifiable_model' => env('LARAVEL_TOOLKIT_NOTIFICATION_USER_MODEL', 'App\Models\User'),
+        'use_prefix' => env('LARAVEL_TOOLKIT_NOTIFICATION_USE_PREFIX', true),
+    ],
 ];
