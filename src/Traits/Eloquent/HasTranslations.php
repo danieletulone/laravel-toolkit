455e325e6a0c36955fba682d263e27c58a6705b1
danieletulone <danieletulone.work@gmail.com>
2023-06-23 14:55:58 +0200

    add HasTranslations trait

---
+++ src/Traits/Eloquent/HasTranslations.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Danieletulone\LaravelToolkit\Traits\Eloquent;
+
+use Spatie\Translatable\HasTranslations as BaseHasTranslations;
+
+trait HasTranslations
+{
+    use BaseHasTranslations;
+
+    public function toArray()
+    {
+        $attributes = parent::toArray();
+
+        foreach ($this->getTranslatableAttributes() as $field) {
+            $attributes[$field] = $this->getTranslation($field, app()->getLocale());
+        }
+
+        return $attributes;
+    }
+}
