3a59a681230d74a8ce648cc06b92394deb87938e
danieletulone <danieletulone.work@gmail.com>
2023-06-24 16:17:57 +0200

    update config

--- config/config.php
+++ config/config.php
@@ -9,5 +9,11 @@ return [
         'notifiable_model' => env('LARAVEL_TOOLKIT_NOTIFICATION_USER_MODEL', 'App\Models\User'),
         'use_prefix' => env('LARAVEL_TOOLKIT_NOTIFICATION_USE_PREFIX', true),
         'notifiable_lang_field' => env('LARAVEL_TOOLKIT_NOTIFICATION_USER_LANG_FIELD', 'lang'),
+        'mail' => [
+            'default_view' => env('LARAVEL_TOOLKIT_NOTIFICATION_MAIL_DEFAULT_VIEW', 'laravel-toolkit::mail'),
+        ],
+        'firebase' => [
+            'credentials' => env('LARAVEL_TOOLKIT_FIREBASE_CREDENTIALS', storage_path('firebase_credentials.json')),
+        ],
     ],
 ];
